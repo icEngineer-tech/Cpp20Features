c32e7008f3ab09d8f108dab008e386035fd9a32b
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-09-30 09:04:32 +0100

    Update three_way_comp.cpp

--- three_way_comp.cpp
+++ three_way_comp.cpp
@@ -2,7 +2,7 @@
 #include <compare>
 #include <numeric>
 
-template <class T, std::enable_if_t<std::is_floating_point_v<T> || std::is_integral_v<T>,bool> =true>
+template <class T, std::enable_if_t<std::is_floating_point_v<T> || std::is_integral_v<T>, bool> = true>
 class Point
 {
 	T x, y;
@@ -10,19 +10,19 @@ public:
 	Point(T x, T y) { this->x = x; this->y = y; }
 	std::strong_ordering operator<=>(const Point& p1)const
 	{
-		auto aveThis= std::midpoint(x, y); // (1) here I'm using midpoint(which is a feature of C++20 <numeric>) to get the average 
+		auto aveThis = std::midpoint(x, y); // (1) here I'm using midpoint(which is a feature of C++20 <numeric>) to get the average 
 		auto aveExt = std::midpoint(p1.x, p1.y);// (2) of the current object(the idea behinds that is to only compare one param)
-		auto res = aveThis <=> aveExt;
-		//auto comp1 = this->y <=> p1.y;	// you can use this instead if (1) and (2)
+		return aveThis <=> aveExt;
+		//auto comp1 = this->y <=> p1.y;	// you can use this instead of (1) and (2)
 		//auto comp2= x <=> p1.x;
-		if (res < 0)
+		/*if (res < 0)
 			return std::strong_ordering::less;
 		else if (res > 0)
 			return std::strong_ordering::greater;
-		else if(res == 0)
-			return std::strong_ordering::equal;
+		else if (res == 0)
+			return std::strong_ordering::equal;*/
 	}
-	
+
 	friend std::ostream& operator<<(std::ostream& os, std::strong_ordering s);
 };
 
@@ -37,7 +37,7 @@ int main()
 	Point p(1, 2);		//template deduction
 	Point p2(2, 1);
 	std::cout << std::boolalpha;
-	std::cout << (p < p2);
+	std::cout << (p <= p2);
 
 	return 0;
 }
