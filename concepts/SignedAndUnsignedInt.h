50eacfa7dbd416ca9b47c33e592ede1e55902718
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-10-27 19:21:57 +0100

    Update SignedAndUnsignedInt.h

--- concepts/SignedAndUnsignedInt.h
+++ concepts/SignedAndUnsignedInt.h
@@ -13,9 +13,6 @@ concept SignedInt = std::is_same_v<T, signed int>; //or std::is_signed_v<T>
 * Unsigned Int 
 */
 template<typename T>
-concept UnsignedInt = requires
-{
-	std::is_same_v<T, unsigned int>; //or std::is_unsigned_v<T>
-};
+concept UnsignedInt = std::is_same_v<T, unsigned int>;// && sizeof(T) == sizeof(unsigned int); //or std::is_unsigned_v<T>
 
 #endif
