5a8993b7cf29ab16ceae7e9c5ae86cc1e8bae49d
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-10-28 15:28:17 +0100

    Update myModule.h

--- concepts/myModule.h
+++ concepts/myModule.h
@@ -9,11 +9,26 @@ concept is_integer = SignedInt<T> || UnsignedInt<T>;
 
 enum class Odd{Odd,Even};
 template <typename T>
-bool isOdd(T x);
+bool isOdd(T x)
+{
+	if constexpr (is_integer<T>)  /*constexpr is primordial here otherwise, you will get an error: C2296 % illegal, left operand hat type T
+					                      for instance, if T is double then % doesn't work for this type. constexpr make that the if is coputed at compile 
+					                      time. is_integer<> is a concept then it's computed at compile time*/
+	{
+		if (x % 2)
+			return true;
+		return false;
+	}
+	throw std::exception();
+}
 /*
 * get oddity
 */
 template <typename T>
-Odd getOddity(T x);
+Odd getOddity(T x)
+{
+	try { if (isOdd(x)) return Odd::Odd; return Odd::Even; }
+	catch (const std::exception& e) { std::cout << e.what(); }
+}
 
 #endif
